2fa2fb3ebc36c1399856c56246f176f57efd0eb4
John Doe <anilkaphen@gmail.com>
2023-10-15 18:44:28 +0100

    Added test case

--- src/main/java/org/twenty092023/FindPrimeNumber.java
+++ src/main/java/org/twenty092023/FindPrimeNumber.java
@@ -2,15 +2,25 @@ package org.twenty092023;
 
 public class FindPrimeNumber {
     public static void main(String args[]) {
-        for (int i = 100; i <= 200; i++) {
-            int c = 0;
-            for (int j = 1; j <= i; j++) {
-                if (i % j == 0)
-                    c++;
+        FindPrimeNumber obj = new FindPrimeNumber();
+        obj.findPrimeNumberForGivenRange(2, 11);
+    }
+
+    public void findPrimeNumberForGivenRange(int start, int end) {
+        for (int i = start; i <= end; i++) {
+            if (isPrimeNumber(i)) {
+                System.out.println("Given number is prime: "+ i);
             }
-            if (c == 2)
-                System.out.println(i);
         }
     }
 
+    public boolean isPrimeNumber(int number) {
+        int c = 0;
+        for (int j = 2; j <= number/2; j++) {
+            if (number % j == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
---
+++ src/test/java/org/twenty092023/FindPrimeNumberTest.java
@@ -0,0 +1,12 @@
+package org.twenty092023;
+
+public class FindPrimeNumberTest {
+
+    FindPrimeNumber findPrimeNumber;
+
+    void test() {
+        findPrimeNumber = new FindPrimeNumber();
+        findPrimeNumber.findPrimeNumberForGivenRange(2,3);
+        findPrimeNumber.isPrimeNumber(8);
+    }
+}
