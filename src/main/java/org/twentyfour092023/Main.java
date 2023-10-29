281c5281f1297c3c213c4b3252ce136b598ae0af
John Doe <anilkaphen@gmail.com>
2023-10-29 12:25:12 +0000

    Added Junit

--- src/main/java/org/twentyfour092023/Main.java
+++ src/main/java/org/twentyfour092023/Main.java
@@ -1,16 +1,16 @@
 package org.twentyfour092023;
 
 public class Main {
-    public static void main(String [] args){
+    public static void main(String[] args) {
         Calculator calculator = new Calculator();
 
-     int resultadd =    calculator.add(5,6);
+        int resultadd = calculator.add(5, 6);
         System.out.println("Addition " + resultadd);
-        int resultminus =    calculator.subtract(10,6);
+        int resultminus = calculator.subtract(10, 6);
         System.out.println("Minus " + resultminus);
-        int resultmultiply =    calculator.multiply(5,6);
+        int resultmultiply = calculator.multiply(5, 6);
         System.out.println("Multiply " + resultmultiply);
-        int resultdivide =    calculator.divide(10,2);
+        int resultdivide = calculator.divide(10, 2);
         System.out.println("Divide " + resultdivide);
     }
 }
