0e5efbf1ee5dd7684945cbed6632eae6e555feb1
Anupamkaphen <Anupamkaphen@gmail.com>
2023-11-06 13:33:36 +0530

    Junit added

--- src/test/java/org/november1st2023/GradeSystemTest.java
+++ src/test/java/org/november1st2023/GradeSystemTest.java
@@ -1,5 +1,6 @@
 package org.november1st2023;
 
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,38 +11,44 @@ public class GradeSystemTest {
     private GradeSystem gradeSystem;
 
     @Before
-    public void setup() {
+    public void setup(){
         gradeSystem = new GradeSystem();
     }
 
     @Test
     public void CalcPercentGradeForAbove75() {
-        Assert.assertEquals(gradeSystem.CalcPercentGrade(75, 75, 75, 75, 75), 75, 76);
-
+        Assert.assertEquals(gradeSystem.calcPercentGrade(75,80,78,92,95),84);
     }
 
     @Test
-    public void CalcPercentGradeForAbove60() {
-
-        Assert.assertEquals(gradeSystem.CalcPercentGrade(65, 65, 65, 65, 65), 65, 76);
-
-
+    public void CalcPercentGradeForAbove75ButoneSubjectBelow40() {
+        Assert.assertEquals(gradeSystem.calcPercentGrade(75,80,38,92,95),76);
     }
 
     @Test
-    public void CalcPercentGradeForAbove50() {
-
-        Assert.assertEquals(gradeSystem.CalcPercentGrade(55, 55, 55, 55, 55), 55, 76);
-
-
+    public void CalcPercentGradeForAbove40Below50() {
+        Assert.assertEquals(gradeSystem.calcPercentGrade(48,54,53,45,50),50);
     }
 
     @Test
-    public void CalcPercentGradeForBelow50() {
-
-        Assert.assertEquals(gradeSystem.CalcPercentGrade(45, 45, 45, 45, 45), 45, 46);
-
-
+    public void CalcPercentGradeForAbove40Below50neSubjectBelow40Marks() {
+        Assert.assertEquals(gradeSystem.calcPercentGrade(40,45,45,45,45),44);
+    }
+    @Test
+    public void CalcPercentGradeForabove40Below50() {
+        Assert.assertEquals(gradeSystem.calcPercentGrade(45,45,45,45,45),45);
+    }
+    @Test
+    public void CalcPercentGradeForAbove60Below75() {
+        Assert.assertEquals(gradeSystem.calcPercentGrade(65,70,68,62,70),67);
+    }
+    @Test
+    public void CalcPercentGradeForAbove60Below75OneSubjectBelow40() {
+        Assert.assertEquals(gradeSystem.calcPercentGrade(35,70,68,62,70),61);
     }
 
+    @Test
+    public void CalcPercentGradeForABelow40() {
+        Assert.assertEquals(gradeSystem.calcPercentGrade(35,30,25,40,20),30);
+    }
 }
