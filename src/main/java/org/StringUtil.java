ddd5692452b48e9424df8a99311fd9fe1579d0b3
John Doe <anilkaphen@gmail.com>
2023-11-08 07:50:17 +0000

    string program

---
+++ src/main/java/org/StringUtil.java
@@ -0,0 +1,10 @@
+package org;
+
+public class StringUtil {
+    public static void main(String[] args) {
+        String s="pankaj";
+        for(char c:s.toCharArray()){
+            System.out.println(c);
+        }
+    }
+}
