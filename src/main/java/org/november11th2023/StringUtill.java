8ec89afa5959ab927e24edd9fcb1b0e7d167b1d2
Anupamkaphen <Anupamkaphen@gmail.com>
2023-11-11 12:28:10 +0530

    String pro

---
+++ src/main/java/org/november11th2023/StringUtill.java
@@ -0,0 +1,31 @@
+package org.november11th2023;
+public class StringUtill {
+    public static void main(String[] args) {
+        StringUtill stringUtill = new StringUtill();
+        substringCount("aa bb cc dd ee dd mm nn ss","dd");
+      stringUtill.palindrome("rotor");
+    }
+    private static void substringCount(String input, String substing){
+    String arr[] = input.split(" ");
+        int count=0;
+        for(String str:arr){
+            if(str.equals(substing))
+            {
+                count++;
+            } }
+        System.out.println(count);
+    }
+
+    private  void palindrome(String s)
+    {
+        String r="";
+        char []a = s.toCharArray();
+        for(int i=a.length-1;i>=0;--i){
+            r=r+String.valueOf(a[i]);
+        } if(s.equals(r)){
+            System.out.println("Palindrome");
+        }
+        else { System.out.println("not Palindrome");
+        } }}
+
+
