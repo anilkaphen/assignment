c883dccc2d95dbe982eb64bf80c700ff5f8a5d42
Anupamkaphen <Anupamkaphen@gmail.com>
2023-12-07 12:18:30 +0530

    Space remove

--- src/main/java/org/collection2/CollectionExample.java
+++ src/main/java/org/collection2/CollectionExample.java
@@ -1,9 +1,6 @@
 package org.collection2;
-
 import java.util.*;
-
 public class CollectionExample {
-
     public static void main(String[] args) {
         List<Integer> list = getAddNumber();
 
@@ -11,12 +8,7 @@ public class CollectionExample {
         List<Integer> list1 =  getLinkedList();
         list1.forEach(s -> System.out.println(s));
         Map<Integer, String> list2 = getHahMap();
-
-
-
     }
-
-
     private static List<Integer> getAddNumber(){
         List<Integer> list = new ArrayList<>();
         list.add(1);
@@ -35,15 +27,13 @@ public static List<Integer> getLinkedList(){
     list.add(0);
     return list;
 }
-    public static List<Integer> getTreeList(){
+    public static TreeSet<Integer> getTreeList(){
         TreeSet<Integer> list1 = new TreeSet<Integer>();
-
         list1.add(0);
         list1.add(1);
         list1.add(2);
         list1.add(5);
         list1.add(4);
-
         return list1;
     }
     public static Map<Integer, String> getHahMap(){
@@ -57,7 +47,5 @@ public static List<Integer> getLinkedList(){
         map.put(7, "hen");
         map.put(1, "apple1");
         return map;
-
     }
-
 }
